a280ac5d9ea117184e5f0762824eade69144d23c
creativemindcanada <creativemindcanada@gmail.com>
2025-02-13 20:07:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -504,7 +504,7 @@ Contact Information:
         st.error(f"An unexpected error occurred: {str(e)}")
         return None
         # Website Analysis
-else:  
+# else:  
     st.subheader("Website Analysis")
     website_url = st.text_input("Enter Website URL for Analysis")
     
