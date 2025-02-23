f1d2ae554d6a2e7bae37acef954b3bdd609fa3c4
creativemindcanada <creativemindcanada@gmail.com>
2025-02-23 16:23:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from bs4 import BeautifulSoup
